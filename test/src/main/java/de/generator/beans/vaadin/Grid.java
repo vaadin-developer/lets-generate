fd2e7b3d3fb95725c2df6ec3915cf3e422e7525a
Daniel Nordhoff-Vergien <dve@vergien.net>
2019-04-25 17:49:27 +0200

    nicer colum

--- test/src/main/java/de/generator/beans/vaadin/Grid.java
+++ test/src/main/java/de/generator/beans/vaadin/Grid.java
@@ -4,15 +4,25 @@ import org.rapidpm.vgu.vaadin.FilterGrid.FilterBuilder;
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.HasValue;
 import com.vaadin.flow.component.HasValue.ValueChangeEvent;
+import com.vaadin.flow.component.grid.Grid.Column;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import de.generator.beans.Address;
+import de.generator.beans.Contract;
 import de.generator.beans.filter.ContractFilter;
 
 @Route("grid")
 public class Grid extends Composite<VerticalLayout> {
   public Grid() {
-    ContractGrid contractGrid = new ContractGrid();
+    ContractGrid contractGrid = new ContractGrid() {
+      @Override
+      protected Column<Contract> createAddressColumn() {
+        Column<Contract> addColumn = getGrid().addColumn(
+            contract -> contract.getAddress() != null ? contract.getAddress().computeCaption()
+                : "");
+        return addColumn;
+      }
+    };
     contractGrid.setBaseQueries(new ContractQueries());
     contractGrid.setAddressBaseQueries(new AddressQueries());
     contractGrid.setSizeFull();
